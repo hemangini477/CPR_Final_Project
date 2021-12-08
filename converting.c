244203ae8e1800db8f7ff7da8796103f53fa5e09
hemangini477 <hemanginipatel022@gmail.com>
2021-12-08 00:47:57 -0500

    Add converting c file for version 3

--- converting.c
+++ converting.c
@@ -56,4 +56,25 @@ void converting()
 	}
 
 	printf("*** End of Converting Strings to double Demo ***\n\n");
+
+    // Project: Version 3
+    // converting long strings to long
+    printf("*** Start of Converting Strings to long Demo ***\n");
+
+	char long_string[80];
+	long long_number;
+
+	printf("Type the long numeric string (q - to quit): \n");
+	gets(long_string);
+
+	// define loop to convert input strings to long until quit
+	while (strcmp(long_string, "q") != 0) // compare input string with q
+	{
+		long_number = atol(long_string); // convert string to double
+		printf("Converted number is %ld\n", long_number);
+		printf("Type the long numeric string (q - to quit): \n");
+		gets(long_string);
+	}
+
+	printf("*** End of Converting Strings to long Demo ***\n\n");
 }
